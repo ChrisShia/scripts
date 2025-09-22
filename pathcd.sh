80cfd588a18c533966db5cca3c945a5135cd3be0
ChrisShia <christos.shiakas@gmail.com>
2025-09-22 09:43:52 +0300

    main:
    Initial commit.
    Added pathcd.sh with working script. Needs refactoring and testing.

---
+++ pathcd.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+#$1 flavour
+#$2 language
+#$3 project
+
+TO="$HOME"
+
+for fDir in $TO/*; do    # list directories in the form "$TO/dirname/"
+    fDir=${fDir%*/}      # remove the trailing "/"
+    fDir="${fDir##*/}"    # print everything after the final "/"
+    if [[ "$fDir" == "$1" ]]; then   
+        TO="${TO}/${fDir}"
+	    for lanDir in $TO/*; do
+           lanDir="${lanDir%*/}"
+	       lanDir="${lanDir##*/}" 
+	       if [[ "$lanDir" == "$2" ]]; then
+	    	    TO="${TO}/${lanDir}"
+	    	    break
+	       fi
+   	    done
+	    break
+    fi
+done
+
+echo "$TO" 
+
+
+find_dir_under_root() {
+    local root=$1
+    local search=$2
+
+    for dir in $root/*; do 
+        dir=
+    done
+
+}
